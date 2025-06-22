f1796380c87e49ee22305e62d47762c009550fff
rajis31 <rajsolan91@gmail.com>
2025-06-22 09:06:52 -0700

    Added in main page

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,10 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::get('/donate', function () {
-    return view('donate'); 
-})->name('donate');
+Route::get('/support-research', function () {
+    return Inertia::render('SupportResearch');
+})->name('support.research');
 
 
 require __DIR__.'/auth.php';
